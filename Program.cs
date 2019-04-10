f4342f7c5c3450ef63c630bb81f77f375e0736cc
MaryiaLa <48055189+MaryiaLa@users.noreply.github.com>
2019-04-10 10:02:04 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Messaging;
-using System.Net.Mime;
-using System.Drawing;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace MessageQueues
+namespace MQListener
 {
 	class Program
 	{
@@ -26,30 +24,12 @@ namespace MessageQueues
 			Console.WriteLine("Enter queue name:");
 			queueName = Console.ReadLine();
 
-			MonitorDirectory(directoryPath);
+			ReceiveMessage(directoryPath, queueName);
 
 			Console.ReadLine();
 		}
 
-		private static void MonitorDirectory(string path)
-		{
-			FileSystemWatcher fileSystemWatcher = new FileSystemWatcher();
-			fileSystemWatcher.Path = path;
-			fileSystemWatcher.Created += FileSystemWatcher_Created;
-			//fileSystemWatcher.Changed += FileSystemWatcher_Changed;
-
-			fileSystemWatcher.EnableRaisingEvents = true;
-
-		}
-
-		private static void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
-		{
-			string filePath = directoryPath + @"\"+ e.Name;
-			Console.WriteLine("File {0} added.", filePath);
-			AddFileToQueue(filePath, queueName);
-		}
-
-		private static void AddFileToQueue(string filePath, string nameOfQueue)
+		private static void ReceiveMessage(string filePath, string nameOfQueue)
 		{
 			MessageQueue queue;
 
@@ -62,28 +42,19 @@ namespace MessageQueues
 				queue = MessageQueue.Create(nameOfQueue);
 			}
 
-			//Image myImage = Image.FromFile(filePath);
-
-			/*MemoryStream stream = new MemoryStream();
-			IFormatter formatter = new BinaryFormatter();
-			formatter.Serialize(stream, file);*/
+			queue.Formatter = new BinaryMessageFormatter();
 
 			using (queue)
-			using (Stream fileStream = new FileStream(filePath, FileMode.Open))
 			{
-				Message message = new Message()
-				{
-					BodyStream = fileStream,
-					Label = Path.GetFileName(filePath),
-					Priority = MessagePriority.Normal,
-					Formatter = new BinaryMessageFormatter()
-				};
-			  //Message message = new Message("file");
+				var message = queue.Receive();
+				message.Formatter = new BinaryMessageFormatter();
+				var reader = new StreamReader(message.BodyStream, Encoding.Unicode);
+				var msgBody = reader.ReadToEnd();
 
-
-				queue.Send(message);
+				/*IFormatter formatter = new BinaryFormatter();
+				stream.Seek(0, SeekOrigin.Begin);
+				object o = formatter.Deserialize(stream);*/
 			}
-
 		}
 	}
 }
